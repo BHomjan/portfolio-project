97d9ec68edb6f3372badfa525f898ef6d85accf0
Manish Bhomjan <bhomjanmanish4@gmail.com>
2025-02-24 17:45:58 +0545

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/portfolio-project/", // Replace with your repository name
   plugins: [react()],
 });
