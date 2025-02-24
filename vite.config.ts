398a7ddff5290d3ea8fee2424d964e32b63703ab
Manish Bhomjan <bhomjanmanish4@gmail.com>
2025-02-24 18:36:32 +0545

    Deploy updated build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio-project/", // Replace with your repository name
+  base: "/portfolio-project/", // Replace with your GitHub repository name
   plugins: [react()],
 });
+
 
